e96f8245a668fb587498701c544e71ad197ac575
Rohit Kumar <askrohit854@gmail.com>
2026-07-23 09:45:19 +0530

    Time: 1 ms (26.87%) | Memory: 9.4 MB (54.53%) - LeetSync

---
+++ 62-unique-paths/unique-paths.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+
+    int numberOfUniqpath(int i, int j, vector<vector<int>>& dp){
+        if(i==0 && j == 0 ) return 1;
+        if(i < 0 || j < 0) return 0;
+
+        if(dp[i][j] != -1)   { return dp[i][j]; }
+
+        int up = numberOfUniqpath(i, j-1 , dp);
+        int left = numberOfUniqpath(i-1, j, dp);
+        return dp[i][j] = up + left;
+    }
+    int uniquePaths(int m, int n) {
+        vector<vector<int>>dp(m, vector<int>(n,-1));
+        return numberOfUniqpath(m-1,n-1, dp);
+    }
+};
