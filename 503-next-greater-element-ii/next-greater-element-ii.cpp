eefcbc4feaecde991e5fa3366e66ca3f5bfcc495
Rohit Kumar <askrohit854@gmail.com>
2026-07-27 00:42:13 +0530

    Time: 106 ms (5.45%) | Memory: 27.9 MB (97.73%) - LeetSync

--- 503-next-greater-element-ii/next-greater-element-ii.cpp
+++ 503-next-greater-element-ii/next-greater-element-ii.cpp
@@ -1,24 +1,43 @@
 class Solution {
 public:
     vector<int> nextGreaterElements(vector<int>& nums) {
+
+
+    // Optimal approach -> O(4 * n)  and O(2 * n)
+        // int n = nums.size();
+
+        // vector<int> ans(n);
+        // stack<int> st;
+
+        // for(int i = 2*n - 1; i >= 0; i--){
+        //     while(!st.empty() && st.top() <= nums[i % n]){
+        //         st.pop();
+        //     }
+        //     if(i < n){
+        //         if(st.empty()){
+        //             ans[i] = -1;
+        //         }
+        //         else{
+        //             ans[i] = st.top();
+        //         }
+        //     }
+        //     st.push(nums[i % n]);
+        // }
+        // return ans;
+
+
         int n = nums.size();
+        vector<int> ans(n, -1);
 
-        vector<int> ans(n);
-        stack<int> st;
+        for(int i = 0; i < n; i++){
+            for(int j = i+1 ; j <= i + n - 1;  j++){
+                int idx = j % n;
 
-        for(int i = 2*n - 1; i >= 0; i--){
-            while(!st.empty() && st.top() <= nums[i % n]){
-                st.pop();
-            }
-            if(i < n){
-                if(st.empty()){
-                    ans[i] = -1;
-                }
-                else{
-                    ans[i] = st.top();
+                if(nums[idx] > nums[i]){
+                    ans[i] = nums[idx];
+                    break;
                 }
             }
-            st.push(nums[i % n]);
         }
         return ans;
     }
