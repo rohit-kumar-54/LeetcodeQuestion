afbe584f8ea2aabcde03440f92de04adbd7a6a1a
Rohit Kumar <askrohit854@gmail.com>
2026-08-01 22:29:28 +0530

    Time: 0 ms (100.00%) | Memory: 14.8 MB (99.81%) - LeetSync

--- 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.cpp
+++ 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.cpp
@@ -11,37 +11,63 @@
 class Solution {
 public:
     ListNode* removeNthFromEnd(ListNode* head, int n) {
-        
-        // Brute force approach
-        int cnt = 0;
-        ListNode* temp = head;
-        // count number of node in our LL
-        while(temp != NULL){
-            cnt++;
-            temp = temp -> next;
 
+        // Optimal Approach -> by reaching before the node to be deleting
+        // using slow and fast pointer
+
+        ListNode* fast = head;
+        for(int i=0; i<n; i++){
+            fast = fast -> next;
         }
-        // if size == N , delete nth node from last -> means delete head
-        if(cnt == n){
+
+        if(fast == NULL){
             ListNode* newHead = head -> next;
             delete head;
             return newHead;
+
+        }
+        ListNode* slow = head;
+        while(fast -> next != NULL){
+            slow = slow -> next;
+            fast = fast -> next;
         }
 
-        // reach before the node which is to be deleted
-        int res = cnt - n;
-        temp = head;
-        while(temp != NULL){
-            res--;
-
-            if(res == 0){
-                break;
-            }
-            temp = temp -> next;
-        }
-        ListNode* delNode = temp -> next;
-        temp -> next = temp -> next -> next;
+        ListNode* delNode = slow -> next;
+        slow -> next = slow -> next -> next;
         delete delNode;
         return head;
+        
+        // Brute force  -> T.C = O(2 * len) and S.C = O(1)
+
+        // int cnt = 0;
+        // ListNode* temp = head;
+        // // count number of node in our LL
+        // while(temp != NULL){
+        //     cnt++;
+        //     temp = temp -> next;
+
+        // }
+        // // if size == N , delete nth node from last -> means delete head
+        // if(cnt == n){
+        //     ListNode* newHead = head -> next;
+        //     delete head;
+        //     return newHead;
+        // }
+
+        // // reach before the node which is to be deleted
+        // int res = cnt - n;
+        // temp = head;
+        // while(temp != NULL){
+        //     res--;
+
+        //     if(res == 0){
+        //         break;
+        //     }
+        //     temp = temp -> next;
+        // }
+        // ListNode* delNode = temp -> next;
+        // temp -> next = temp -> next -> next;
+        // delete delNode;
+        // return head;
     }
 };
