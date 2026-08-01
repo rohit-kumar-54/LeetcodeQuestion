cb835ffc03a878bd5fa9c453358bc8c6ddae75f8
Rohit Kumar <askrohit854@gmail.com>
2026-08-01 21:36:37 +0530

    Time: 4 ms (48.96%) | Memory: 124.3 MB (32.78%) - LeetSync

--- 234-palindrome-linked-list/palindrome-linked-list.cpp
+++ 234-palindrome-linked-list/palindrome-linked-list.cpp
@@ -11,32 +11,67 @@
 class Solution {
 public:
     ListNode *reverseList(ListNode* head){
-        ListNode *curr = head;
-        ListNode *prev = NULL;
-        while(curr != NULL){
-            ListNode *next = curr -> next;
-            curr -> next = prev;
-            prev = curr;
-            curr = next;
-        } 
-        return prev;
+        if(head == NULL || head -> next == NULL) return head;
+
+        ListNode* newHead = reverseList(head -> next);
+        ListNode* front = head -> next;
+        front -> next = head;
+        head -> next = NULL;
+        return newHead;
+
+
+        // ListNode *curr = head;
+        // ListNode *prev = NULL;
+        // while(curr != NULL){
+        //     ListNode *next = curr -> next;
+        //     curr -> next = prev;
+        //     prev = curr;
+        //     curr = next;
+        // } 
+        // return prev;
     }
     bool isPalindrome(ListNode* head) {
-        if(head == NULL) return true;
-        ListNode  *slow = head , *fast = head;
-        while(fast->next != NULL && fast->next->next != NULL){
+        if(head == NULL && head -> next == NULL) return true;
+
+        // find Middle node to reverse 2nd half part
+        ListNode* slow = head;
+        ListNode* fast = head;
+        while(fast -> next != NULL && fast -> next -> next != NULL){
             slow = slow -> next;
             fast = fast -> next -> next;
-        }
-        ListNode *rev = reverseList(slow->next);
-        ListNode *curr = head;
-        while(rev != NULL){
-            if(rev -> val != curr -> val){
+        }// here we find middle or 2nd middle node at slow -> next;
+
+        // reverse 2nd half
+        ListNode* newHead = reverseList(slow -> next);
+        ListNode* first = head;
+        ListNode* second = newHead;
+
+        while(second != NULL){
+            if(first -> val != second -> val){
+                reverseList(newHead);
                 return false;
             }
-            rev = rev -> next;
-            curr = curr -> next;
+            first  = first -> next;
+            second = second -> next;
         }
+        reverseList(newHead);
         return true;
+
+        // if(head == NULL) return true;
+        // ListNode  *slow = head , *fast = head;
+        // while(fast->next != NULL && fast->next->next != NULL){
+        //     slow = slow -> next;
+        //     fast = fast -> next -> next;
+        // }
+        // ListNode *rev = reverseList(slow->next);
+        // ListNode *curr = head;
+        // while(rev != NULL){
+        //     if(rev -> val != curr -> val){
+        //         return false;
+        //     }
+        //     rev = rev -> next;
+        //     curr = curr -> next;
+        // }
+        // return true;
     }
 };
