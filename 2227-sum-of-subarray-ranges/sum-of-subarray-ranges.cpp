29232166340500f1e0a2aaec146d098381ea6b77
Rohit Kumar <askrohit854@gmail.com>
2026-08-18 03:25:58 +0530

    Time: 0 ms (100.00%) | Memory: 15.9 MB (42.02%) - LeetSync

--- 2227-sum-of-subarray-ranges/sum-of-subarray-ranges.cpp
+++ 2227-sum-of-subarray-ranges/sum-of-subarray-ranges.cpp
@@ -1,26 +1,262 @@
-class Solution {
+class Solution 
+{
 public:
 
-    long long subArrayRanges(vector<int>& nums) {
+    vector<int> findNSE(vector<int> &arr) {
+        
+        // Size of array
+        int n = arr.size();
+        
+        // To store the answer
+        vector<int> ans(n);
+        
+        // Stack 
+        stack<int> st;
+        
+        // Start traversing from the back
+        for(int i = n - 1; i >= 0; i--) {
+            
+            // Get the current element
+            int currEle = arr[i];
+            
+            /* Pop the elements in the stack until 
+            the stack is not empty and the top 
+            element is not the smaller element */
+            while(!st.empty() && arr[st.top()] >= currEle){
+                st.pop();
+            }
+            
+            // Update the answer
+            ans[i] = !st.empty() ? st.top() : n;
+            
+            /* Push the index of current 
+            element in the stack */
+            st.push(i);
+        }
+        
+        // Return the answer
+        return ans;
+    }
+
+
+     vector<int> findNGE(vector<int> &arr) {
+        
+        // Size of array
+        int n = arr.size();
+        
+        // To store the answer
+        vector<int> ans(n);
+        
+        // Stack 
+        stack<int> st;
+        
+        // Start traversing from the back
+        for(int i = n - 1; i >= 0; i--) {
+            
+            // Get the current element
+            int currEle = arr[i];
+            
+            /* Pop the elements in the stack until 
+            the stack is not empty and the top 
+            element is not the greater element */
+            while(!st.empty() && arr[st.top()] <= currEle){
+                st.pop();
+            }
+            
+            // Update the answer
+            ans[i] = !st.empty() ? st.top() : n;
+            
+            /* Push the index of current 
+            element in the stack */
+            st.push(i);
+        }
+        
+        // Return the answer
+        return ans;
+    }
+
+     /* Function to find the indices of 
+    previous smaller or equal elements */
+    vector<int> findPSEE(vector<int> &arr) {
+        
+        // Size of array
+        int n = arr.size();
+        
+        // To store the answer
+        vector<int> ans(n);
+        
+        // Stack 
+        stack<int> st;
+        
+        // Traverse on the array
+        for(int i=0; i < n; i++) {
+            
+            // Get the current element
+            int currEle = arr[i];
+            
+            /* Pop the elements in the stack until 
+            the stack is not empty and the top 
+            elements are greater than the current element */
+            while(!st.empty() && arr[st.top()] > currEle){
+                st.pop();
+            }
+            
+            // Update the answer
+            ans[i] = !st.empty() ? st.top() : -1;
+            
+            /* Push the index of current 
+            element in the stack */
+            st.push(i);
+        }
+        
+        // Return the answer
+        return ans;
+    }
+
+    /* Function to find the indices of 
+    previous greater or equal elements */
+    vector<int> findPGEE(vector<int> &arr) {
+        
+        // Size of array
+        int n = arr.size();
+        
+        // To store the answer
+        vector<int> ans(n);
+        
+        // Stack 
+        stack<int> st;
+        
+        // Traverse on the array
+        for(int i=0; i < n; i++) {
+            
+            // Get the current element
+            int currEle = arr[i];
+            
+            /* Pop the elements in the stack until 
+            the stack is not empty and the top 
+            elements are smaller than the current element */
+            while(!st.empty() && arr[st.top()] < currEle){
+                st.pop();
+            }
+            
+            // Update the answer
+            ans[i] = !st.empty() ? st.top() : -1;
+            
+            /* Push the index of current 
+            element in the stack */
+            st.push(i);
+        }
+        
+        // Return the answer
+        return ans;
+    }
+    /* Function to find the sum of the 
+    minimum value in each subarray */
+    long long sumSubarrayMins(vector<int> &arr) {
+        
+        vector<int> nse = findNSE(arr);
+        
+        vector<int> psee = findPSEE(arr);
+        
+        // Size of array
+        int n = arr.size();
+        
+        // To store the sum
+        long long sum = 0;
+        
+        // Traverse on the array
+        for(int i=0; i < n; i++) {
+            
+            // Count of first type of subarrays
+            int left = i - psee[i];
+            
+            // Count of second type of subarrays
+            int right = nse[i] - i;
+            
+            /* Count of subarrays where 
+            current element is minimum */
+            long long freq = left*right*1LL;
+            
+            // Contribution due to current element 
+            long long val = (freq*arr[i]*1LL);
+            
+            // Updating the sum
+            sum += val;
+        }
+        
+        // Return the computed sum
+        return sum;
+    }
+
+    /* Function to find the sum of the 
+    maximum value in each subarray */
+    long long sumSubarrayMaxs(vector<int> &arr) {
+        
+        vector<int> nge = findNGE(arr);
+        
+        vector<int> pgee = findPGEE(arr);
+        
+        // Size of array
+        int n = arr.size();
+        
+        // To store the sum
+        long long sum = 0;
+        
+        // Traverse on the array
+        for(int i=0; i < n; i++) {
+            
+            // Count of first type of subarrays
+            int left = i - pgee[i];
+            
+            // Count of second type of subarrays
+            int right = nge[i] - i;
+            
+            /* Count of subarrays where 
+            current element is minimum */
+            long long freq = left*right*1LL;
+            
+            // Contribution due to current element 
+            long long val = (freq*arr[i]*1LL);
+            
+            // Updating the sum
+            sum += val;
+        }
+        
+        // Return the computed sum
+        return sum;
+    }
+
+
+    
+
+
+    long long subArrayRanges(vector<int>& arr) {
+
+         return ( sumSubarrayMaxs(arr) - sumSubarrayMins(arr) );
+
 
         
 
 
     // Brute force Approach
-        int n = nums.size();
-        long long total_sum  = 0;
-        for(int i = 0; i < n; i++){
-            int mini = INT_MAX;
-            int maxi = INT_MIN;
+    //     int n = nums.size();
+    //     long long total_sum  = 0;
+    //     for(int i = 0; i < n; i++){
+    //         int mini = INT_MAX;
+    //         int maxi = INT_MIN;
 
-            for(int j = i; j<n; j++ ){
-                mini = min(mini, nums[j] );
-                maxi = max(maxi, nums[j]);
+    //         for(int j = i; j<n; j++ ){
+    //             mini = min(mini, nums[j] );
+    //             maxi = max(maxi, nums[j]);
 
-                long long diff = maxi - mini;
-                total_sum += diff;
-            }
-        }
-        return total_sum;
+    //             long long diff = maxi - mini;
+    //             total_sum += diff;
+    //         }
+    //     }
+    //     return total_sum;
+    
     }
+
+
+
 };
