c5470a56b7fda1954713ae1d141a5feb119be298
Rohit Kumar <askrohit854@gmail.com>
2026-08-21 16:31:12 +0530

    Time: 473 ms (85.08%) | Memory: 0B (100.00%) - LeetSync

--- 584-find-customer-referee/find-customer-referee.sql
+++ 584-find-customer-referee/find-customer-referee.sql
@@ -1,2 +1,12 @@
 # Write your MySQL query statement below
-select name from Customer where referee_id != 2 OR referee_id is null;
+-- select name from Customer where referee_id != 2 OR referee_id is null;
+
+
+-- Another way to write this query
+
+SELECT
+    name
+FROM
+    Customer
+WHERE 
+    IFNULL(referee_id , 0) != 2;
