27676b4a395d15ac5b923039ea1d95804b7aa4c6
Rohit Kumar <askrohit854@gmail.com>
2026-08-21 16:40:57 +0530

    Time: 352 ms (22.26%) | Memory: 0B (100.00%) - LeetSync

--- 595-big-countries/big-countries.sql
+++ 595-big-countries/big-countries.sql
@@ -1,7 +1,28 @@
 # Write your MySQL query statement below
+-- SELECT 
+--     name, population , area
+-- FROM 
+--     World
+-- WHERE
+--     area >= 3000000 OR population >= 25000000;
+
+
+
+
+-- Method 2 -> using UNION
+
 SELECT 
     name, population , area
 FROM 
     World
 WHERE
-    area >= 3000000 OR population >= 25000000;
+    area >= 3000000 
+
+UNION
+
+SELECT 
+    name, population , area
+FROM 
+    World
+WHERE
+    population >= 25000000;
