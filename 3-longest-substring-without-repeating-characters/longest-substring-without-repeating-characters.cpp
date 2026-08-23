c6ec2ac8aaccb6bf4557c0b4be17a672a1b19d53
Rohit Kumar <askrohit854@gmail.com>
2026-08-23 15:25:54 +0530

    Time: 0 ms (100.00%) | Memory: 18.9 MB (39.49%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
@@ -1,22 +1,48 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        
-        int maxLen = 0;
 
-        for(int i=0; i < s.length(); i++){
-            int hash[256] = {0};
-            
 
-            for(int j=i; j<s.length(); j++){
-                if(hash[s[j]] == 1) break;
+    // Optimal Approach : Using two pointer approach and Sliding window
 
-                int len = j - i + 1;
+        int hash[256] ;
+        for(int i=0; i<256; i++){
+            hash[i] = {-1};
+        }
+        int n = s.size();
+        int l = 0, r = 0, maxLen = 0;
 
-                maxLen = max(maxLen, len);
-                hash[s[j]] = 1;
+        while(r < n){
+            if(hash[s[r]] != -1){
+                if(hash[s[r]] >= l){
+                    l = hash[s[r]] + 1;
+                }
             }
+            int len = r - l + 1;
+            maxLen = max(maxLen, len);
+
+            hash[s[r]] = r;
+            r++;
         }
         return maxLen;
+        
+
+    // Brute force -> O(n * n) and S.C = O(256)
+        // int maxLen = 0;
+
+        // for(int i=0; i < s.length(); i++){
+        //     int hash[256] = {0};
+            
+
+        //     for(int j=i; j<s.length(); j++){
+        //         if(hash[s[j]] == 1) break;
+
+        //         int len = j - i + 1;
+
+        //         maxLen = max(maxLen, len);
+        //         hash[s[j]] = 1;
+        //     }
+        // }
+        // return maxLen;
     }
 };
