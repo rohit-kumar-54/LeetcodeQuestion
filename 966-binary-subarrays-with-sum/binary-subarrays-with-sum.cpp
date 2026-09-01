8bc944f75d3f46d70fbd752b54dbfeebde7e6a26
Rohit Kumar <askrohit854@gmail.com>
2026-09-01 23:23:54 +0530

    Time: 0 ms (100.00%) | Memory: 32.7 MB (40.02%) - LeetSync

--- 966-binary-subarrays-with-sum/binary-subarrays-with-sum.cpp
+++ 966-binary-subarrays-with-sum/binary-subarrays-with-sum.cpp
@@ -2,50 +2,70 @@ class Solution {
 public:
     int numSubarraysWithSum(vector<int>& nums, int goal) {
 
-
-    // Better approach -> using map and prefSum and use freq of pref_sum - goal
-
+    //  Approach : using two pointer and sliding windows
+    //  T.C = O(2 * n)   and S.C = 0;    
         int n = nums.size();
-        // hashmap to store prefSum and their frequency 
-        map<int, int> prefSum;
-        int currSum = 0;
+        int left = 0, right = 0;
         int cnt = 0;
-        prefSum[0] = 1;
+        
+        int windowSum = 0;
+        int countZeros = 0;
 
-        for(int num : nums){
-            currSum += num;
-            int remSum = currSum - goal;
+        while(right < n){
+            windowSum += nums[right];
+            
+            while(left < right && (nums[left] == 0 ||  windowSum > goal)){
 
-            if(prefSum.find(remSum) != prefSum.end()){
-                cnt = cnt + prefSum[remSum];
+                if(nums[left] == 0){
+                    countZeros++;
+                }
+                else{
+                    countZeros = 0;
+                }
+
+                windowSum = windowSum - nums[left];
+                left++;
             }
-            prefSum[currSum]++;
+            if(windowSum == goal){
+                cnt = cnt + 1;
+                cnt = cnt + countZeros;
+            }
+
+            right++;
         }
+        
         return cnt;
 
 
-    // // Better Approach : T.C = O()   and S.C = 0;    
-    //     int n = nums.size();
-    //     int left = 0, right = 0;
-    //     int cnt = 0;
-        
-    //     int sum = 0;
-    //     while(right < n){
-    //         sum += nums[right];
+    // Better approach -> using map and prefSum and use freq of pref_sum - goal
+    // Approach: (1)- initialize a map to store freq of prefSum seen so far
+    // (2)--initialize variables for curr prefSum[0] = 1 and cnt =0 , currSum = 0;
+    // (3) -- iterate array 
+    //          -- at each step , update the currSum += nums[i];
+    //          -- check (prefSum - currSUm) exist in map , if yes add freq of that ele in cnt
+    // update the prefSUm and 
+    // last return cnt;
 
-    //         if(sum ==  goal){
-    //             cnt++;
-    //         }
-            
-    //         while(sum > goal){
-    //             sum = sum - nums[left];
-    //             left++;
-    //         }
+        // int n = nums.size();
+        // // hashmap to store prefSum and their frequency 
+        // map<int, int> prefSum;
+        // int currSum = 0;
+        // int cnt = 0;
+        // prefSum[0] = 1;
 
-    //         right++;
-    //     }
-        
-    //     return cnt;
+        // for(int num : nums){
+        //     currSum += num;
+        //     int remSum = currSum - goal;
+
+        //     if(prefSum.find(remSum) != prefSum.end()){
+        //         cnt = cnt + prefSum[remSum];
+        //     }
+        //     prefSum[currSum]++;
+        // }
+        // return cnt;
+
+
+   
 
     // Brute force : T.C = O(n * n) and S.C = O(1) 
     // using two for loop and check every subarray sum , if equal count increases
