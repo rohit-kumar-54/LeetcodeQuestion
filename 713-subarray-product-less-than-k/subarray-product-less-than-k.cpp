b14ac657096f4ffee9605d0803046f4ae3c3f10e
Rohit Kumar <askrohit854@gmail.com>
2026-09-11 02:48:25 +0530

    Time: 0 ms (100.00%) | Memory: 44.8 MB (66.28%) - LeetSync

--- 713-subarray-product-less-than-k/subarray-product-less-than-k.cpp
+++ 713-subarray-product-less-than-k/subarray-product-less-than-k.cpp
@@ -2,22 +2,40 @@ class Solution {
 public:
     int numSubarrayProductLessThanK(vector<int>& nums, int k) {
         
+        // using sliding window 
+        if(k <= 1) return 0;  // b/c 
+
+        int left = 0;
+        int count = 0;
+        int prod = 1;
+
+        for(int right = 0; right < nums.size(); right++){
+            prod *= nums[right];
+
+            while(prod >= k){
+                prod /=  nums[left];    // divide because product ko kam karna hai
+                left++;
+            }
+            int windowLength = right - left + 1;
+            count += windowLength;
+        }
+        return count;
 
     // // brete force - O(n * n) and O(1)
             // base case
-        if (k <= 1) return 0;
-        int n = nums.size();
-        int cnt =0;
-        for(int i=0; i < n; i++){
-            long long prod = 1;
-            for(int j = i  ; j < n; j++){
-                prod *= nums[j];
+        // if (k <= 1) return 0;
+        // int n = nums.size();
+        // int cnt =0;
+        // for(int i=0; i < n; i++){
+        //     long long prod = 1;
+        //     for(int j = i  ; j < n; j++){
+        //         prod *= nums[j];
 
-                if(prod < k) cnt++;
+        //         if(prod < k) cnt++;
 
-                else break;
-            }
-        }
-        return cnt;
+        //         else break;
+        //     }
+        // }
+        // return cnt;
     }
 };
