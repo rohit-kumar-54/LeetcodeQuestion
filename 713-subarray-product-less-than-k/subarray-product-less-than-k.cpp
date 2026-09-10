bdccdf04dbc7bce7338d01aa537170ce72f4714a
Rohit Kumar <askrohit854@gmail.com>
2026-09-11 02:28:35 +0530

    Time: 654 ms (6.33%) | Memory: 44.9 MB (6.57%) - LeetSync

--- 713-subarray-product-less-than-k/subarray-product-less-than-k.cpp
+++ 713-subarray-product-less-than-k/subarray-product-less-than-k.cpp
@@ -2,6 +2,10 @@ class Solution {
 public:
     int numSubarrayProductLessThanK(vector<int>& nums, int k) {
         
+
+    // // brete force - O(n * n) and O(1)
+            // base case
+        if (k <= 1) return 0;
         int n = nums.size();
         int cnt =0;
         for(int i=0; i < n; i++){
