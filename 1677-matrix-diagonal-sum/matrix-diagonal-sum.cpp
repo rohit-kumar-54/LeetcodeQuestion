6793ecf75187f348b7c19b13e1007cabf28944f0
Rohit Kumar <askrohit854@gmail.com>
2026-09-23 03:21:11 +0530

    Time: 0 ms (100.00%) | Memory: 15.1 MB (15.47%) - LeetSync

--- 1677-matrix-diagonal-sum/matrix-diagonal-sum.cpp
+++ 1677-matrix-diagonal-sum/matrix-diagonal-sum.cpp
@@ -1,22 +1,34 @@
 class Solution {
 public:
     int diagonalSum(vector<vector<int>>& mat) {
-        int n = mat.size();
-        int m = mat[0].size();
-        int sum = 0;
-        
-        for(int i=0; i<n; i++){
-            for(int j=0; j<m; j++){
-                if( i == j ){  // Primary Diagonal 
-                    sum += mat[i][j];
-                }
 
-                else if( i+j == n-1){  // Secondary Diagonal
-                    sum += mat[i][j];
-                }
-            }
+        int n = mat.size();
+        int sum = 0;
+
+        for(int i=0; i<n; i++){
+            if( i == i) sum += mat[i][i];
+            if ( i != n - i - 1) sum += mat[i][n-i-1];
         }
         return sum;
+
+
+
+        // int n = mat.size();
+        // int m = mat[0].size();
+        // int sum = 0;
+        
+        // for(int i=0; i<n; i++){
+        //     for(int j=0; j<m; j++){
+        //         if( i == j ){  // Primary Diagonal 
+        //             sum += mat[i][j];
+        //         }
+
+        //         else if( i+j == n-1){  // Secondary Diagonal
+        //             sum += mat[i][j];
+        //         }
+        //     }
+        // }
+        // return sum;
        
     }
 };
